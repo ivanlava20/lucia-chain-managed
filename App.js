9bcd8e9dc41887c2b3cd69c8dc9632e72f25da2b
Ivan Lava <84676951+ivanlava20@users.noreply.github.com>
2022-03-27 20:43:46 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import ResolveAuthScreen from "./src/screens/ResolveAuthScreen";
+import SignInScreen from "./src/screens/SignInScreen";
+import FeedScreen from "./src/screens/FeedScreen";
+
+export default function App() {
+  return (
+    <FeedScreen/>
+  );
+}
+
+
